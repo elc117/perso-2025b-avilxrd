175d97f4595067d79827ee2b9fbe5990251516fa
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2025-09-17 14:32:27 -0300

    feat: adiciona funcao que filtra as mensagens entre dois usuarios

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Text (Text)
+import GHC.Generics
+
+data Message = Message
+    {   
+        -- msg_read    :: Bool,
+        -- msg_id      :: Int,    
+        user_from   :: Text,
+        user_to     :: Text,
+        msg_content :: Text
+    } deriving (Show, Generic, Eq)
+
+-- recebe dois usuarios e uma lista de mensagens
+-- retorna as mensagens entre os dois usuarios
+filter_messages :: Text -> Text -> [Message] -> [Message]
+filter_messages user1 user2 all_messages = filter pred all_messages
+    where 
+        pred msg = 
+            (user_from msg == user1 && user_to msg == user2) ||
+            (user_from msg == user2 && user_to msg == user1) 
