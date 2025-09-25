e59346ba9d7eb1beed73d283e2927ae9045ec157
Miguel Avila <miguelaviladeoliveira@gmail.com>
2025-09-24 21:30:10 -0300

    feat: adiciona funções para recuperar dados do .db e modifica rotas para utilizar do .db

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-
 module Main (main) where
 
-import Web.Scotty
 import Data.IORef
+import Web.Scotty
 import Data.Text (Text)
 import qualified Data.Text.Lazy    as LT
 import qualified Data.Text.Lazy.IO as TIO
@@ -26,14 +25,14 @@ data Message = Message
 instance ToJSON   Message
 instance FromJSON Message
 
+instance FromRow Message where
+    fromRow = Message <$> field <*> field <*> field
+
+
+-- a função foi descontinuada pois agora é feito um filtro no SQL
 -- recebe dois usuarios e uma lista de mensagens
 -- retorna as mensagens entre os dois usuarios
-filter_messages :: Text -> Text -> [Message] -> [Message]
-filter_messages user1 user2 all_messages = filter pred all_messages
-    where 
-        pred msg = 
-            (user_from msg == user1 && user_to msg == user2) ||
-            (user_from msg == user2 && user_to msg == user1) 
+-- filter_messages :: Text -> Text -> [Message] -> [Message]
 
 -- inicializa o banco de dados
 init_db :: Connection -> IO ()
@@ -50,10 +49,22 @@ insert_message conn msg = execute conn
     "INSERT INTO messages (user_from, user_to, msg_content) VALUES (?,?,?)"
     (user_from msg, user_to msg, msg_content msg)
 
+-- recupera todas as mensagens
+get_all_messages :: Connection -> IO [Message]
+get_all_messages conn = query_ conn "SELECT user_from, user_to, msg_content FROM messages" :: IO [Message]
+
+-- recupera as mensagens entre dois usuarios
+get_chat :: Connection -> Text -> Text -> IO [Message]
+get_chat conn user1 user2 = do
+    query conn
+        "SELECT user_from, user_to, msg_content FROM messages WHERE \
+        \ (user_from = ? AND user_to = ?) OR \
+        \ (user_from = ? AND user_to = ?)"
+        (user1, user2, user2, user1) :: IO [Message]
+
 
 main :: IO ()
 main = do
-    messages <- newIORef([] :: [Message])
     
     conn <- open "messages.db"
     init_db conn
@@ -69,19 +80,18 @@ main = do
         -- rota para envio de mensagens
         post "/msg" $ do
             new_message <- jsonData
-            liftIO $ modifyIORef messages (new_message :)
+            liftIO $ insert_message conn new_message
             json new_message
         
         -- rota para recuperar todas as mensagens
         get "/msgs" $ do
-            all_messages <- liftIO $ readIORef messages
+            all_messages <- liftIO $ get_all_messages conn
             json all_messages
 
         -- rota para recuperar as mensagens entre dois usuarios
         get "/chat/:user1/:user2" $ do
             user1 <- param "user1"
             user2 <- param "user2"
-            all_messages <- liftIO $ readIORef messages
+            all_messages <- liftIO $ get_chat conn user1 user2
 
-            -- filter_messages definida em Lib.hs
-            json (filter_messages user1 user2 all_messages)
+            json all_messages
